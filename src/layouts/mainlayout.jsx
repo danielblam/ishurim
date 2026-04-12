cdc57bd093ad7a30d06fe735f2b850f8548d539b
Daniel Blam <danielblam29@gmail.com>
2026-04-12 09:54:21 +0300

    Updated UI colors to be a bit nicer

--- src/layouts/mainlayout.jsx
+++ src/layouts/mainlayout.jsx
@@ -1,24 +1,38 @@
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Link, Outlet } from "react-router-dom";
 
 export function MainLayout() {
     return (
         <div className="main-layout-container row">
             <div className="col-10 pe-0">
-                <div className="top-bar bg-dark">
+                <div className="top-bar bg-secondary">
                     things to put here: logged in as (user), extra buttons, etc.
                 </div>
-                <Outlet/>
+                <Outlet />
             </div>
-            <div className="main-dashboard col-2 vh-100">
+            <div className="main-dashboard col-2 vh-100 bg-dark text-white">
                 <nav className="main-dashboard-nav">
-                    <h1>תפריט ראשי</h1>
-                    <div><Link to="/dash/approvals?create=true">אישור חדש</Link></div>
-                    <div><Link to="/dash/approvals?create=false">רשימת אישורים</Link></div>
-                    <div><Link to="/dash/approvers">רשימת מאשרים</Link></div>
-                    <div><Link to="/dash/tests">סוגי בדיקות</Link></div>
-                    <div><Link to="/dash/vehicles">כלי תחבורה</Link></div>
-                    <div><Link to="/dash/institutes">מכונים</Link></div>
-                    <div><Link to="/dash/hospitals">בתי חולים</Link></div>
+                    <h2>תפריט ראשי</h2>
+                    <div><NavLink to="/dash/approvals?create=true">
+                        אישור חדש
+                    </NavLink></div>
+                    <div><NavLink to="/dash/approvals?create=false" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        רשימת אישורים
+                    </NavLink></div>
+                    <div><NavLink to="/dash/approvers" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        רשימת מאשרים
+                    </NavLink></div>
+                    <div><NavLink to="/dash/tests" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        סוגי בדיקות
+                    </NavLink></div>
+                    <div><NavLink to="/dash/vehicles" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        כלי תחבורה
+                    </NavLink></div>
+                    <div><NavLink to="/dash/institutes" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        מכונים
+                    </NavLink></div>
+                    <div><NavLink to="/dash/hospitals" className={({ isActive }) => isActive ? "current-page" : ""}>
+                        בתי חולים
+                    </NavLink></div>
                 </nav>
             </div>
         </div>
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
-import './main.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import './main.css'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
