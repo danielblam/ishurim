7fb9855e71f830c0ded58fa43626bfacc8998bf1
Daniel Blam <danielblam29@gmail.com>
2026-04-19 14:43:06 +0300

    Api URL is now in a config file

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import App from './App.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.min.js';
 import './main.css'
+import { initUrl } from './services/utils.js';
+
+initUrl()
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
--- src/services/utils.js
+++ src/services/utils.js
@@ -1,1 +1,7 @@
-export const url = 'https://localhost:7063/api'
+export var url = null; // = 'https://localhost:7063/api'
+
+export async function initUrl() {
+    const response = await fetch("config.json")
+    const json = await response.json()
+    url = json.apiUrl
+}
