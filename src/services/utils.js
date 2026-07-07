bd8df33cb4b335e88d8bd1311d9bb1a85631934c
Daniel Blam <danielblam29@gmail.com>
2026-07-07 12:25:08 +0300

    Page refresh bug fixed

--- src/services/utils.js
+++ src/services/utils.js
@@ -1,7 +1,7 @@
 export var url = null; // = 'https://localhost:7063/api'
 
 export async function initUrl() {
-    const response = await fetch("config.json")
+    const response = await fetch("/config.json")
     const json = await response.json()
     url = json.apiUrl
 }
